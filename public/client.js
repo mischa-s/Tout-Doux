15fcb86cc5bed1f5bf3fa502b0e5314e730e9626
mischa-saunders <mischa.saunders@gmail.com>
2017-01-10 11:05:08 +1300

    update all cats display

--- public/client.js
+++ public/client.js
@@ -12,3 +12,19 @@ const data = [
    age:9
   }
 ]
+
+
+document.addEventListener('DOMContentLoaded', function(e) {
+  $.get('api/v1/cats/')
+  .done(function(response){
+    $('.resources').append(template(response)
+  })
+
+  $('#submit').on("click", event => {
+    event.preventDefault()
+    const input = $('#form').serializeArray()
+    $.post('/api/v1/cats', input, funciton(res){
+      $('.resources').append(template(response))
+    })
+  }
+})
